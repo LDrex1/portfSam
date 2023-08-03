43d6beb9450eace74aab3c18ea7ab5114775bce5
lDrex1 <damzealwini7@gmail.com>
2023-08-03 11:50:22 +0100

    new: file for styling variables and types

---
+++ src/lib/style-variables/styleVar.ts
@@ -0,0 +1,15 @@
+import { type } from "os";
+
+export type ColorsType =
+  | "primary1"
+  | "primary2"
+  | "dark"
+  | "bright"
+  | "grey"
+  | "#4859F2";
+
+export type FontSizeType = "h1" | "h2" | "h3" | "link" | "md" | "xl";
+
+export type FontWeightType = "v-high" | "high" | "mid" | "low";
+
+export type Braduis = "lg" | "sm" | "md" | "xl" | "2xl" | "3xl";
