175c85146dc567563b8c3ebb34a904b77b43cfa5
lDrex1 <damzealwini7@gmail.com>
2023-08-03 11:46:48 +0100

    added: Section1, Section2 and Section3 components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+import ImageCard from "@/components/cards/ImageCard";
+import Button from "@/components/custom-comps/Button";
+import Typography, {
+  Body,
+  H1,
+  H2,
+  H3,
+  Title,
+} from "@/components/typography/Typography";
+import { landing, skills, workExp } from "@/lib/landing/landContent";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+  const { greeting, myPicture, cvLink, intro } = landing.section1;
+  const Section1 = () => {
+    return (
+      <section className="bg-bright px-3 md:px-10 lg:px-28">
+        <div className="flex flex-col mt-4 justify-between items-center md:grid md:grid-cols-[300px_minmax(400px,_1fr)_5px] md:grid-flow-dense md:justify-stretch md:gap-x-10 lg:gap-x-28">
+          <ImageCard
+            width={["[80%]", "[200px]", "[500px]"]}
+            src={""}
+            alt={""}
+          />
+          <div>
+            <H1 text={greeting} />
+            <Body text={intro} />
+            <Button
+              styTail="mt-5"
+              text="Download CV"
+              background={"primary1"}
+              bRadius={"lg"}
             />
-          </a>
+          </div>
         </div>
-      </div>
+      </section>
+    );
+  };
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const Section2 = () => {
+    return (
+      <section className="py-6 md:h-[50vw] md:py-10 py-15 bg-bright-yellow grid grid-cols-5 lg:px-28 items-center gap-x-6 justify-between lg: px-4">
+        <div className="col-span-2">
+          <H3 color="primary2" text="My Skills" />
+          <Body text={intro} />
+        </div>
+        <div className="col-span-3 grid sm:grid-cols-2 md:gap-x-4 lg:gap-x-6 lg:gap-y-6">
+          {skills.map(({ skill, description, icon }) => (
+            <div className="bg-white px-7 md:px-6 lg:px-8 py-3 md:py-6 lg:py-8">
+              <Title
+                styTail="text-center"
+                medium
+                color="primary2"
+                text={skill}
+              />
+              <Body
+                styTail="py-3 md:py-5 lg:py-7"
+                color="grey"
+                text={description}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  };
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+  const Section3 = () => {
+    return (
+      <section className="bg-white flex flex-col justify-center py-6 md:h-[50vw] md:py-10 py-15 lg:px-28">
+        <div className="md:grid grid-cols-2">
+          <div className="">
+            <H3 text="Work Experience" />
+            <div className="mt-6 flex flex-col lg:gap-y-9">
+              {workExp.map(({ company, role, startDate, endDate, logo }) => (
+                <div key={company} className="flex gap-x-3">
+                  <span>logo</span>
+                  <div>
+                    <Typography
+                      text={company}
+                      textType={"light-text"}
+                      fontSize={"link"}
+                      fontWeight={"v-high"}
+                    />
+                    <Body text={role} />
+                    <Body text={`${startDate} - ${endDate}`} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="justify justify-self-end">
+            <H3 text="Education" />
+            <div className="mt-6 flex flex-col lg:gap-y-9">
+              {workExp.map(({ company, role, startDate, endDate, logo }) => (
+                <div key={company} className="flex gap-x-3">
+                  <span>logo</span>
+                  <div>
+                    <Typography
+                      text={"Kwame Nkrumah University"}
+                      textType={"light-text"}
+                      fontSize={"xl"}
+                      fontWeight={"v-high"}
+                    />
+                    <Body text={role} />
+                    <Body text={`${startDate} - ${endDate}`} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  return (
+    <main className="">
+      <Section1 />
+      <Section2 />
+      <Section3 />
     </main>
-  )
+  );
 }
