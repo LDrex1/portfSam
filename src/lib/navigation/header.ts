96db00631f9fdc510867feaca571a7c95f93fccc
lDrex1 <damzealwini7@gmail.com>
2023-08-03 11:50:00 +0100

    new: file to house header contents

---
+++ src/lib/navigation/header.ts
@@ -0,0 +1,33 @@
+export type SubLink = {
+  name: string;
+  href: string;
+  tag: string;
+};
+
+export const navLinks: SubLink[] = [
+  {
+    name: "projects",
+    href: "",
+    tag: "",
+  },
+  {
+    name: "skills",
+    href: "",
+    tag: "",
+  },
+  {
+    name: "testimonials",
+    href: "",
+    tag: "",
+  },
+  {
+    name: "resume",
+    href: "",
+    tag: "",
+  },
+  {
+    name: "contact",
+    href: "",
+    tag: "",
+  },
+];
