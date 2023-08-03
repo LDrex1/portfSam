d6f14f4b56bd41dfac0ab00d86952ce89c495e72
lDrex1 <damzealwini7@gmail.com>
2023-08-03 11:44:51 +0100

    added: colors and fontSizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: {},
+      colors: {
+        primary1: "#0C3247",
+        primary2: "#012760",
+        bright: "#F5F5F5",
+        dark: "#121212",
+        grey: "#5B6061",
+        "bright-yellow": "#FFF9EE",
+      },
+      fontSize: {
+        md: "1.25rem",
+        xl: "3rem",
+        "2xl": "3.75rem",
+        "3xl": "4rem",
       },
     },
   },
   plugins: [],
-}
+};
