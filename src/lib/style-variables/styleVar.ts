bb8c416c153a2bf29c513a909dd178e1f171ff4e
lDrex1 <damzealwini7@gmail.com>
2023-08-04 23:34:43 +0100

    update: ColorsType and FontSizeType

--- src/lib/style-variables/styleVar.ts
+++ src/lib/style-variables/styleVar.ts
@@ -5,10 +5,24 @@ export type ColorsType =
   | "primary2"
   | "dark"
   | "bright"
+  | "white"
   | "grey"
+  | "purple"
+  | "light-blue"
+  | "sky-blue"
+  | "bright-yellow"
   | "#4859F2";
 
-export type FontSizeType = "h1" | "h2" | "h3" | "link" | "md" | "xl";
+export type FontSizeType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "link"
+  | "md"
+  | "sub-head"
+  | "xl"
+  | "2xl"
+  | "3xl";
 
 export type FontWeightType = "v-high" | "high" | "mid" | "low";
 
