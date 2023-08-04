84a2aa5afbaa2ccf4dd5b3d275fabaeefad42f83
lDrex1 <damzealwini7@gmail.com>
2023-08-04 23:31:39 +0100

    new file for saving pictures info to be rendered in the gallery

---
+++ src/components/gallery/pictures.ts
@@ -0,0 +1,56 @@
+export type PicturesProp = {
+  image: string;
+  colSpan: `sm:col-span-${number}`;
+  rowSpan: `sm:row-span-${number}`;
+  alt: string;
+};
+export const pictures: PicturesProp[] = [
+  {
+    image: "",
+    colSpan: `sm:col-span-2`,
+    rowSpan: `sm:row-span-4`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-2`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-1`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-2`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-1`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-2`,
+    rowSpan: `sm:row-span-3`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-2`,
+    alt: "",
+  },
+  {
+    image: "",
+    colSpan: `sm:col-span-1`,
+    rowSpan: `sm:row-span-2`,
+    alt: "",
+  },
+];
