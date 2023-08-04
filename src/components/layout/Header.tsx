1a68e51d60e60418e7b5a230d736cfe6dded63fa
lDrex1 <damzealwini7@gmail.com>
2023-08-04 23:32:03 +0100

    updated: header, ul and li classNames

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,13 +4,13 @@ import { H3, Title } from "../typography/Typography";
 
 function Header() {
   return (
-    <header className="z-10 -mt-4 px-3 lg:px-28 bg-bright font-medium md:fixed w-full">
+    <header className="z-10 py-5 px-3 lg:px-28 bg-bright font-medium md:fixed w-full">
       <nav className="absolute md:flex md:static justify-between items-center font-medium py-3 uppercase text-primary2">
         <Title medium text={"Samuel Ogbonna"} />
         <div>
-          <ul className="flex flex-col md:flex-row gap-y-2 md:gap-x-4">
+          <ul className="flex flex-col md:flex-row gap-y-2 md:gap-y-0 md:gap-x-10 lg:gap-x-14">
             {navLinks.map(({ name, href }) => (
-              <li className="text-sm">
+              <li className="text-base font-medium">
                 <Link href={href}>{name}</Link>
               </li>
             ))}
