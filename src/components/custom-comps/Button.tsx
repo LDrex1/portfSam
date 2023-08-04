dcbc59b101578c00fb9a2a2d921e6c972add8618
lDrex1 <damzealwini7@gmail.com>
2023-08-04 23:30:35 +0100

    update: CclassName

--- src/components/custom-comps/Button.tsx
+++ src/components/custom-comps/Button.tsx
@@ -16,12 +16,18 @@ export function Button({
   medium,
   styTail,
 }: ButtonProp) {
-  const className = `bg-${
-    background[0] === "#" ? `[${background}]` : background
-  }  text-${color} ${
-    medium && "font-medium"
-  } py-3 px-4 rounded-s-3xl rounded-e-3xl ${styTail}`;
-  return <button className={className}>{text}</button>;
+  //   const bg = background[0] !== "#" ? background : `[${background}]`;
+  //   const textColor = color || "";
+  //   const mid = (medium && "font-medium") || "";
+  const purpleButton = `bg-purple text-white font-bold py-3 px-6 rounded-s-3xl rounded-e-3xl ${
+    styTail || ""
+  }`;
+  const className = background === "purple" ? purpleButton : "";
+  return (
+    <button type="button" className={className}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
