2cb72f02f9672d4908123b9b5e0ed11b821be70d
lDrex1 <damzealwini7@gmail.com>
2023-08-08 00:05:59 +0100

    update: projects route and dynamic props

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,9 +1,25 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { Body, Title } from "../typography/Typography";
 import ImageCard from "./ImageCard";
+import { Project } from "@/lib/landing/landContent";
 
-function ProjectCard({ styTail }: { styTail?: string }) {
+function ProjectCard({
+  styTail,
+  project,
+}: {
+  styTail?: string;
+  project: Project;
+}) {
+  const router = useRouter();
+  const handleRoute = (): void => {
+    router.push(`/projects/${project.title}`);
+  };
   return (
-    <div className="bg-primary1-white px-4 pt-5 rounded-2xl flex flex-col flex-1 items-center md:flex-row md:items-stretch md:gap-x-6 lg:gap-x-10 md:px-8 md:pt-10">
+    <div
+      onClick={handleRoute}
+      className="px-4 bg-primary1 bg-opacity-60 pt-5 rounded-xl md:rounded-3xl flex flex-col flex-1 items-center md:flex-row md:items-stretch md:gap-x-6 lg:gap-x-10 md:px-8 md:pt-10"
+    >
       <ImageCard
         styTail={styTail}
         rounded="t-2xl"
@@ -12,17 +28,14 @@ function ProjectCard({ styTail }: { styTail?: string }) {
         aspect="aut"
         type={"long"}
       />
-      <div className="flex flex-col md:gap-y-4 md:justify-self-start">
+      <div className="flex flex-col sm:max-w-[550px] md:gap-y-4 md:justify-self-start lg:max-w-[552px] ">
         <Title
           styTail="text-[24px]"
           medium
           color="text-white"
-          text={"Projects Title"}
+          text={project.title}
         />
-        <Body
-          color="text-light-blue"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore"
-        />
+        <Body color="text-light-blue" text={project.about} />
       </div>
     </div>
   );
