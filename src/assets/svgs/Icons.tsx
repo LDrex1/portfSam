3f4871826b3e4291f2f726e6178a6d9225e0ef7c
lDrex1 <damzealwini7@gmail.com>
2023-08-08 00:04:55 +0100

    new file to export svg icons

---
+++ src/assets/svgs/Icons.tsx
@@ -0,0 +1,4 @@
+export { default as Personnel } from "./Personnel.svg";
+export { default as Puzzle } from "./Puzzle.svg";
+export { default as Cube } from "./Cube.svg";
+export { default as Gear } from "./Gear.svg";
