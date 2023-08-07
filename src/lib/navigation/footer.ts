31b4f3ab6825c2e29e64cdad76172a6b870065a1
lDrex1 <damzealwini7@gmail.com>
2023-08-08 00:09:31 +0100

    added: svg imports and social-media info

--- src/lib/navigation/footer.ts
+++ src/lib/navigation/footer.ts
@@ -1,15 +1,43 @@
 import { SubLink } from "./header";
-
+import { FaLinkedinIn } from "react-icons/fa";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { SiGmail } from "react-icons/si";
+import React, { JSXElementConstructor } from "react";
 type footerContent = {
   catchPhrase: string;
-  socialMedia: Omit<SubLink, "tag">[];
+  socialMedia: (Omit<SubLink, "tag"> & {
+    icon: typeof React.Component;
+    tag: string;
+    iconColor: "text-linkedin" | "text-gmail" | "text-call";
+  })[];
+  mailTo: string;
 };
 
 export const footerContent: footerContent = {
   catchPhrase: "Lets build awesome products together",
   socialMedia: [
-    { name: "LinkedIn", href: "" },
-    { name: "Twitter", href: "" },
-    { name: "Instagram", href: "" },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/aanuoluwapo-abiola-6a4b76101/",
+      icon: FaLinkedinIn,
+      iconColor: "text-linkedin",
+      tag: "",
+    },
+    {
+      name: "Email",
+      href: "mailto:aanu.abiola@gmail.com",
+      icon: SiGmail,
+      iconColor: "text-gmail",
+      tag: "",
+    },
+    {
+      name: "Phone",
+      href: "",
+      icon: BiSolidPhoneCall,
+      iconColor: "text-call",
+      tag: "+123456789",
+    },
+    // { name: "Instagram", href: "" },
   ],
+  mailTo: "aanu.abiola@gmail.com",
 };
