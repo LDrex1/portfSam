cafe7064462d395054f88bc34216ec6db8ff13d0
lDrex1 <damzealwini7@gmail.com>
2023-08-08 00:03:54 +0100

    update: style and skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
+import { Puzzle } from "@/assets/svgs/Icons";
 import ImageCard from "@/components/cards/ImageCard";
 import ProjectCard from "@/components/cards/ProjectCard";
 import Button from "@/components/custom-comps/Button";
 import Gallery from "@/components/gallery/Gallery";
 import Reviews from "@/components/reviews/Reviews";
-// import Reviews from "@/components/reviews/Reviews";
 import Typography, {
   Body,
   H1,
-  H2,
   H3,
   Title,
 } from "@/components/typography/Typography";
-import { landing, skills, workExp } from "@/lib/landing/landContent";
-import Image from "next/image";
+import { landing, projects, skills, workExp } from "@/lib/landing/landContent";
 
 export default function Home() {
   const { greeting, myPicture, cvLink, intro } = landing.section1;
   const Section1 = () => {
     return (
-      <section className="bg-bright pt-20 pb-12 ld:pt-0 lg:flex lg:flex-col lg:justify-center lg:h-[90vh] md:px-10 lg:px-28">
-        <div className="flex flex-col md:gap-x-[calc(14px+4vw)] md:grid-cols-[minmax(350px,_700px)_minmax(300px,_400px)] lg:justify-between items-center md:grid lg:grid-cols-[minmax(200px,_700px)_minmax(200px,_400px)] md:grid-flow-dense lg:gap-x-[calc(14px+7vw)]">
+      <section className="bg-bright px-4 pt-16 sm:pt-20 pb-12 lg:pt-0 lg:flex lg:flex-col lg:justify-center lg:h-[90vh] md:px-10 lg:px-28">
+        <div className="flex flex-col md:gap-x-[calc(14px+4vw)] md:grid-cols-[minmax(350px,_700px)_minmax(300px,_400px)] lg:justify-between items-center md:grid lg:grid-cols-[minmax(200px,_700px)_minmax(200px,_500px)] md:grid-flow-dense lg:gap-x-[calc(14px+7vw)]">
           <div>
             <H1
               styTail="w-fit bg-gradient-to-r from-[#012760] to-[#4859f2] bg-gradient-170 bg-clip-text text-transparent"
@@ -28,7 +26,7 @@ export default function Home() {
             <Body styTail="mt-2 md:mt-3 lg:mt-9" text={intro} />
             <Button
               styTail="mt-5 md:mt-7 mb-9 md:mb-0 ms-8 md:ms-0 lg:mt-16 text-white shadow-shade1"
-              text="Download CV"
+              text="Download Resume"
               background="purple"
               bRadius="lg"
             />
@@ -37,7 +35,7 @@ export default function Home() {
           <ImageCard
             src={""}
             alt={""}
-            styTail="mix-blend-multiply lg:rounded-3xl md:justify-self-center lg:justify-self-end"
+            styTail="mix-blend-multiply  md:rounded-3xl lg:rounded-[90px] md:justify-self-center lg:justify-self-end"
             type={"short"}
           />
         </div>
@@ -52,12 +50,18 @@ export default function Home() {
           <H3 color="text-primary2" text="My Skills" />
           <Body text={intro} />
         </div>
-        <div className="col-span-3 grid mt-4 md:mt-0 sm:grid-cols-2 gap-x-2 gap-y-2 md:gap-x-4 lg:gap-x-6 lg:gap-y-6">
+        <div
+          id="skills"
+          className="col-span-3 grid mt-4 md:mt-0 sm:grid-cols-2 gap-x-2 gap-y-2 md:gap-x-4 lg:gap-x-6 lg:gap-y-6"
+        >
           {skills.map(({ skill, description, icon }) => (
             <div
               key={skill}
-              className="bg-white px-7 md:px-6 lg:px-8 py-3 md:py-6 lg:py-8"
+              className="bg-white flex flex-col items-center px-7 md:px-6 lg:px-8 py-3 md:py-6 lg:py-8"
             >
+              <div className="text-[50px] mb-2 sm:mb-3 md:mb-4 lg:mb-6">
+                {icon}
+              </div>
               <Title
                 styTail="text-center"
                 medium
@@ -65,7 +69,7 @@ export default function Home() {
                 text={skill}
               />
               <Body
-                styTail="py-3 md:py-5 lg:py-7"
+                styTail="py-3 md:py-5 lg:py-7 justify-self-start"
                 color="text-grey"
                 text={description}
               />
@@ -78,13 +82,13 @@ export default function Home() {
 
   const Section3 = () => {
     return (
-      <section className="bg-white px-4 sm:px-6 md:px-8 flex flex-col justify-center py-6 lg:h-[100vh] sm:py-10 md:py-15 lg:px-28">
-        <div className="md:grid grid-cols-2 gap">
-          <div className="">
-            <H3 text="Work Experience" />
+      <section className="bg-white px-4 sm:px-6 md:px-8 flex flex-col py-6 lg:py-20 lg:h-[100vh] sm:py-10 md:py-15 lg:px-28">
+        <div className="sm:grid grid-cols-2 gap">
+          <div id="resume" className="">
+            <H3 text="Work Experience" color="text-primary2" />
 
-            <div className="relative mt-6 flex flex-col gap-y-3 sm:gap-y-6 md:gap-y-7 lg:gap-y-9">
-              <span className="absolute w-1 border-r-2 h-full left-4 top-0"></span>
+            <div className="relative mt-4 md:mt-6 flex flex-col gap-y-3 sm:gap-y-6 md:gap-y-7 lg:gap-y-9">
+              <span className="absolute w-1 border-dashed border-r-2 h-full left-4 top-0"></span>
 
               {workExp.map(({ company, role, startDate, endDate, logo }) => (
                 <div key={company} className="flex gap-x-3">
@@ -104,10 +108,10 @@ export default function Home() {
               ))}
             </div>
           </div>
-          <div className="justify justify-self-end">
-            <H3 text="Education" />
-            <div className="relative mt-6 gap-y-3 sm:gap-y-6 md:gap-y-7 flex flex-col lg:gap-y-9">
-              <span className="absolute w-1 border-r-2 h-full left-4 top-0"></span>
+          <div className="mt-8 sm:mt-0 justify justify-self-end">
+            <H3 text="Education" color="text-primary2" />
+            <div className="relative mt-4 md:mt-6 gap-y-3 sm:gap-y-6 md:gap-y-7 flex flex-col lg:gap-y-9">
+              <span className="absolute w-1 border-r-2 border-dashed h-full left-4 top-0"></span>
 
               {workExp.map(({ company, role, startDate, endDate, logo }) => (
                 <div key={company} className="flex gap-x-3">
@@ -134,10 +138,17 @@ export default function Home() {
 
   const Section4 = () => {
     return (
-      <section className="bg-primary1 px-4 sm:px-6 md:px-8 lg:pt-16 lg:px-28 flex flex-col gap-y-3 md:gap-y-5 lg:gap-y-8">
+      <section
+        id="projects"
+        className="bg-primary1 pt-3 sm:pt-6 md:pt-10 pb-4 sm:pb-8 md:pb-11 lg:pb-20 px-4 sm:px-6 md:px-8 lg:pt-16 lg:px-28 flex flex-col gap-y-3 md:gap-y-5 lg:gap-y-8"
+      >
         <H3 color="text-white" text="Projects" />
-        {[1, 2, 3].map(() => (
-          <ProjectCard styTail="items-center" />
+        {projects.map((project) => (
+          <div className="bg-white-lin rounded-xl md:rounded-3xl">
+            <div className="m-1 bg-dark-rad bg-opacity-30 rounded-xl md:rounded-3xl">
+              <ProjectCard project={project} styTail="items-center" />
+            </div>
+          </div>
         ))}
       </section>
     );
@@ -147,7 +158,8 @@ export default function Home() {
     return (
       <section className="bg-white px-4 sm:px-6 md:px-8 lg:pt-16 lg:px-28 flex flex-col gap-y-3 md:gap-y-5 lg:gap-y-8">
         <Typography
-          color="text-white"
+          styTail="text-[26px] md:text-[30px] lg:text-[40px] font-medium"
+          color="text-primary2"
           text="Sharing some beautiful moments with you"
         />
         <Gallery />
@@ -157,11 +169,14 @@ export default function Home() {
 
   const Section6 = () => {
     return (
-      <section className="bg-sky-blue py-6 sm:py-10 md:py-15 px-4 sm:px-6 md:px-8 lg:pt-16 lg:px-28 flex flex-col md:flex-row lg:gap-x-10">
+      <section
+        id="testimonials"
+        className="bg-sky-blue py-6 sm:py-10 md:py-15 px-4 sm:px-6 md:px-8 lg:pt-16 lg:px-28 flex flex-col md:flex-row md:gap-x-9 lg:gap-x-10"
+      >
         <Typography
-          styTail="w-full"
-          color="text-white"
-          text="Sharing some beautiful moments with you"
+          styTail="w-full shrink-1 grow-1 basis-[50%] sm:min-w-[180px] lg:min-w-[230px] lg:max-w-[450px] text-[26px] md:text-[30px] lg:text-[40px] font-medium"
+          color="text-primary2"
+          text="Hear from cool people I worked with"
         />
         <Reviews />
       </section>
