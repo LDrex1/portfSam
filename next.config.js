ca7ec8e78123fedbc310b25a7bf1738fd760f30e
lDrex1 <damzealwini7@gmail.com>
2023-08-07 23:50:55 +0100

    webpack config for svgr loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     domains: ["tailwindui.com"],
     contentSecurityPolicy: "",
   },
+  reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
