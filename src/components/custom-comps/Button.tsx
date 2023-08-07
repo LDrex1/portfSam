d9f9d2ad084edbda53a6d57783491b877f4d707b
lDrex1 <damzealwini7@gmail.com>
2023-08-08 00:12:27 +0100

    update: props

--- src/components/custom-comps/Button.tsx
+++ src/components/custom-comps/Button.tsx
@@ -1,4 +1,6 @@
 import { Braduis, ColorsType } from "@/lib/style-variables/styleVar";
+import { HtmlProps } from "next/dist/shared/lib/html-context";
+import React from "react";
 
 type ButtonProp = {
   text: string;
@@ -7,6 +9,8 @@ type ButtonProp = {
   color?: ColorsType;
   medium?: boolean;
   styTail?: string;
+  type?: "button" | "submit" | "reset";
+  icon?: React.Component;
 };
 export function Button({
   text,
@@ -15,16 +19,18 @@ export function Button({
   color,
   medium,
   styTail,
-}: ButtonProp) {
+  type,
+  ...rest
+}: ButtonProp & React.HtmlHTMLAttributes<HTMLButtonElement>) {
   //   const bg = background[0] !== "#" ? background : `[${background}]`;
   //   const textColor = color || "";
   //   const mid = (medium && "font-medium") || "";
-  const purpleButton = `bg-purple text-white font-bold py-3 px-6 rounded-s-3xl rounded-e-3xl ${
+  const purpleButton = `bg-purple text-white font-bold px-4 py-2 md:px-4 md:py-3 lg:px-6 rounded-s-3xl rounded-e-3xl ${
     styTail || ""
   }`;
   const className = background === "purple" ? purpleButton : "";
   return (
-    <button type="button" className={className}>
+    <button {...rest} type={type || "button"} className={className}>
       {text}
     </button>
   );
